b3268a148541b17cb63ad28f8a7a91427d351159
tgf <tiago.n.ferreira@gmail.com>
2016-04-06 15:59:13 +0100

    [BACKLOG-7458] use require, share common js w marketplace
    using require from requirejs-osgi-manager
    requirejs wrapping, manual bootstrap
    reusing jquery, angular, angular-route, angular-sanitize
    shim angular-animate

--- plugins/repositories-plugin/src/main/resources/web/js/controllers.js
+++ plugins/repositories-plugin/src/main/resources/web/js/controllers.js
@@ -1,47 +1,58 @@
-var repoConnectionAppControllers = angular.module('repoConnectionAppControllers', []);
+define(
+    [
+        'angular',
+        'angular-sanitize',
+        'repositories/models'
+    ],
 
-repoConnectionAppControllers.controller("RepositorySelectionController", function($scope, repoModel) {
-  $scope.repoModel = repoModel;
-  $scope.selectRepository = function(repository) {
-    repoModel.selectedRepository = repository;
-  }
-  $scope.close = function() {
-    close();
-  }
-  $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
-    var selectedButton = $("#button-"+repoModel.selectedRepository.id);
-    var left = selectedButton.offset().left + selectedButton.outerWidth() / 2;
-    left -= $("#display-box").offset().left;
-    $('#triangle-up').css("left", left - $('#triangle-up').outerWidth() / 2);
-  });
-});
+  function(angular) {
 
-repoConnectionAppControllers.controller("PentahoRepositoryController", function($scope, $location, pentahoRepositoryModel) {
-  $scope.pentahoRepositoryModel = pentahoRepositoryModel;
-  $scope.finish = function() {
-    try {
-      var repo = {
-        "index": 1,
-        "displayName": pentahoRepositoryModel.displayName,
-        "description": pentahoRepositoryModel.description,
-        "url": pentahoRepositoryModel.url
+    var repoConnectionAppControllers = angular.module('repoConnectionAppControllers', []);
+
+    repoConnectionAppControllers.controller("RepositorySelectionController", function($scope, repoModel) {
+      $scope.repoModel = repoModel;
+      $scope.selectRepository = function(repository) {
+        repoModel.selectedRepository = repository;
       }
-      if (createPentahoRepo(JSON.stringify(repo))) {
-        $location.path("/pentaho-repository-2");
+      $scope.close = function() {
+        close();
       }
-    } catch(e) {
-      alert(e);
-    }
-  };
-  $scope.close = function() {
-    close();
+      $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
+        var selectedButton = $("#button-"+repoModel.selectedRepository.id);
+        var left = selectedButton.offset().left + selectedButton.outerWidth() / 2;
+        left -= $("#display-box").offset().left;
+        $('#triangle-up').css("left", left - $('#triangle-up').outerWidth() / 2);
+      });
+    });
+
+    repoConnectionAppControllers.controller("PentahoRepositoryController", function($scope, $location, pentahoRepositoryModel) {
+      $scope.pentahoRepositoryModel = pentahoRepositoryModel;
+      $scope.finish = function() {
+        try {
+          var repo = {
+            "index": 1,
+            "displayName": pentahoRepositoryModel.displayName,
+            "description": pentahoRepositoryModel.description,
+            "url": pentahoRepositoryModel.url
+          }
+          if (createPentahoRepo(JSON.stringify(repo))) {
+            $location.path("/pentaho-repository-2");
+          }
+        } catch(e) {
+          alert(e);
+        }
+      };
+      $scope.close = function() {
+        close();
+      }
+    })
+
+    repoConnectionAppControllers.controller("KettleDatabaseRepositoryController", function() {
+
+    });
+
+    repoConnectionAppControllers.controller("KettleFileRepositoryController", function() {
+
+    });
   }
-})
-
-repoConnectionAppControllers.controller("KettleDatabaseRepositoryController", function() {
-
-});
-
-repoConnectionAppControllers.controller("KettleFileRepositoryController", function() {
-
-});
+);
--- plugins/repositories-plugin/src/main/resources/web/js/models.js
+++ plugins/repositories-plugin/src/main/resources/web/js/models.js
@@ -1,36 +1,43 @@
-repoConnectionApp.service("repoModel", function() {
-  this.repositories = [
-    {
-      "id": 1,
-      "index": "1",
-      "text": "Pentaho Repository",
-      "message": "Enterprise ready storage designed for your business needs.<br /> The Pentaho Repository uses Apache Jackrabbit to store ETL metadata.",
-      "link": "pentaho-repository",
-      "class": "pentaho"
-    },
-    {
-      "id": 2,
-      "index:": "0",
-      "text": "Kettle Database Repository",
-      "message": "A Kettle Database Repository uses a central relational database<br /> to store ETL metadata.",
-      "link": "kettle-database-repository",
-      "class": "kettle-db"
-    },
-    {
-      "id": 3,
-      "index": "2",
-      "text": "Kettle File Repository",
-      "message": "A Kettle file repository uses your local file system<br /> to store ETL metadata.",
-      "link": "kettle-file-repository",
-      "class": "kettle-file"
-    }
-  ]
-  this.selectedRepository = this.repositories[0];
-});
+define( [
+  'repositories'
+  ],
+  function ( repoConnectionApp ) {
+
+    repoConnectionApp.service("repoModel", function() {
+      this.repositories = [
+        {
+          "id": 1,
+          "index": "1",
+          "text": "Pentaho Repository",
+          "message": "Enterprise ready storage designed for your business needs.<br /> The Pentaho Repository uses Apache Jackrabbit to store ETL metadata.",
+          "link": "pentaho-repository",
+          "class": "pentaho"
+        },
+        {
+          "id": 2,
+          "index:": "0",
+          "text": "Kettle Database Repository",
+          "message": "A Kettle Database Repository uses a central relational database<br /> to store ETL metadata.",
+          "link": "kettle-database-repository",
+          "class": "kettle-db"
+        },
+        {
+          "id": 3,
+          "index": "2",
+          "text": "Kettle File Repository",
+          "message": "A Kettle file repository uses your local file system<br /> to store ETL metadata.",
+          "link": "kettle-file-repository",
+          "class": "kettle-file"
+        }
+      ]
+      this.selectedRepository = this.repositories[0];
+    });
 
 
-repoConnectionApp.service("pentahoRepositoryModel",function() {
-  this.displayName = "";
-  this.url = "http://localhost:8080/pentaho-di";
-  this.description = "";
+    repoConnectionApp.service("pentahoRepositoryModel",function() {
+      this.displayName = "";
+      this.url = "http://localhost:8080/pentaho-di";
+      this.description = "";
+    });
+
 });
